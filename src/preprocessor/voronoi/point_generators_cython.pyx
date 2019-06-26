5298fb93c30f2c2f43ee016ed02a3366da9015b2
Vaclav Sadilek <kelidas@centrum.cz>
2019-06-26 15:07:57 +0200

    point generator rewritten in Cython

---
+++ src/preprocessor/voronoi/point_generators_cython.pyx
@@ -0,0 +1,62 @@
+cimport cython
+import numpy as np
+cimport numpy as np
+from libc.stdio cimport printf
+from libc.stdlib cimport rand, RAND_MAX
+from libcpp.vector cimport vector
+from cpprandom cimport mt19937_64, uniform_real_distribution
+
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+@cython.cdivision(True)
+def generateNodesRect_cython(double[:] maxLim,
+                      float minDist,
+                      int dim,
+                      int trials,
+                      list node_coords):
+    print('Generating {:d}d block segment of size: {}'.format(dim, ' / '.join('{:f}'.format(i) for i in maxLim)))
+    cdef:
+        int generatedPoints = 0
+        cdef int p
+        int tr = 0
+        double distInt
+        int i
+        vector[float] coords
+        int node_coords_len
+        vector[float] node_coords_temp
+        bint distIsGood
+        mt19937_64 gen = mt19937_64()
+        uniform_real_distribution[double] dist = uniform_real_distribution[double](0.0,1.0)
+    coords.push_back(0.0)
+    coords.push_back(0.0)
+
+    while (tr < trials):
+        tr = 0
+        distIsGood = False
+        while (not distIsGood) and (tr < trials):
+            for i in range(dim):
+                coords[i] = dist(gen)
+            distIsGood = True
+
+            node_coords_len = generatedPoints
+            for p in range(node_coords_len):
+                distInt = 0
+                for i in range(dim):
+                    distInt += ((node_coords_temp[p*dim+i] - coords[i])*
+                                (node_coords_temp[p*dim+i] - coords[i]))
+                distInt = distInt**0.5
+                if (distInt < minDist):
+                    distIsGood = False
+                    tr += 1
+                    break
+
+        # Adding node coords
+        if distIsGood and (tr < trials):
+            for i in range(dim):
+                node_coords_temp.push_back(coords[i])
+            generatedPoints += 1
+
+    # Copy back to lists
+    for i in range(generatedPoints):
+        node_coords.append([node_coords_temp[i*dim], node_coords_temp[i*dim+1]])
