1c1da6bbc2fb54d17e655c59e060e62431beab3e
kelidas <kelidas@ex07.fce.vutbr.cz>
2019-06-28 15:42:48 +0200

    added numpy includes

--- src/preprocessor/voronoi/setup.py
+++ src/preprocessor/voronoi/setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy as np
 
 setup(ext_modules=cythonize(
            "point_generators_cython.pyx",                 # our Cython source
            # sources=["Rectangle.cpp"],  # additional source file(s)
-           language="c++",             # generate C++ code
-      ))
+           # language="c++",             # generate C++ code - old Cython
+      ),
+      include_dirs = [np.get_include()],
+)
