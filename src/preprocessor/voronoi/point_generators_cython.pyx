8f7860d874d26283ca126b017c86bd8b97bf8a51
Vaclav Sadilek <kelidas@centrum.cz>
2020-08-16 09:03:11 +0200

    added types to cython; added some doc/test strings

--- src/preprocessor/voronoi/point_generators_cython.pyx
+++ src/preprocessor/voronoi/point_generators_cython.pyx
@@ -6,8 +6,9 @@ import random
 from libc.stdio cimport printf
 from libc.stdlib cimport rand, RAND_MAX
 from libcpp.vector cimport vector
-from cpprandom cimport mt19937_64, uniform_real_distribution
+#from cpprandom cimport mt19937_64, uniform_real_distribution
 from libcpp cimport bool
+from libc.math cimport sin, cos, abs
 
 @cython.boundscheck(False)  # Deactivate bounds checking
 @cython.wraparound(False)   # Deactivate negative indexing.
@@ -21,22 +22,24 @@ def generateNodesRect_cython(double[:] maxLim,
     print('Generating {:d}d block segment of size: {}'.format(dim, ' / '.join('{:f}'.format(i) for i in maxLim)))
     cdef:
         int generatedPoints = 0
-        int p, d
+        int p, d, i = 0
         int tr = 0
         double distInt
         vector[double] coords
-        int node_coords_input_len = len(node_coords)
+        int node_coords_input_len = 0
         vector[double] node_coords_temp
+        double[:] topBound, lowBound
         bint distIsGood
-        mt19937_64 gen = mt19937_64()
-        uniform_real_distribution[double] dist = uniform_real_distribution[double](0.0, 1.0)
+        #mt19937_64 gen = mt19937_64()
+        #uniform_real_distribution[double] dist = uniform_real_distribution[double](0.0, 1.0)
     for d in range(dim):
         coords.push_back(0.0)
 
     if node_coords:
-        for node in node_coords:
+        node_coords_input_len = len(node_coords)
+        for i in range(node_coords_input_len):
             for d in range(dim):
-                node_coords_temp.push_back(node[d])
+                node_coords_temp.push_back(node_coords[i][d])
 
     while (tr < trials):
         tr = 0
@@ -46,11 +49,11 @@ def generateNodesRect_cython(double[:] maxLim,
                 topBound = maxLim[0:dim]
                 lowBound = maxLim[dim:2*dim]
                 for d in range(dim):
-                    coords[d] = lowBound[d] + random.random() * (topBound[d]-lowBound[d])
+                    coords[d] = lowBound[d] + np.random.rand() * (topBound[d] - lowBound[d])
 
             else:
                 for d in range(dim):
-                    coords[d] = random.random() * maxLim[d]
+                    coords[d] = np.random.rand() * maxLim[d]
 
             distIsGood = True
 
@@ -111,35 +114,36 @@ def generateNodesOrtoCilinder3dRand_cython(
         vector[double] coords
         int node_coords_input_len = len(node_coords)
         vector[double] node_coords_temp
+        double[:] rn
         bint distIsGood
-        mt19937_64 gen = mt19937_64()
-        uniform_real_distribution[double] dist = uniform_real_distribution[double](0.0, 1.0)
+        #mt19937_64 gen = mt19937_64()
+        #uniform_real_distribution[double] dist = uniform_real_distribution[double](0.0, 1.0)
     for d in range(3):
         coords.push_back(0.0)
 
     if node_coords:
-        for node in node_coords:
-            for d in range(3):
-                node_coords_temp.push_back(node[d])
+        for i in range(node_coords_input_len):
+            for d in range(dim):
+                node_coords_temp.push_back(node_coords[i][d])
 
     while (tr < trials):
         tr = 0
         distIsGood = False
         while (not distIsGood) and (tr < trials):
-            angle = random.random()  * np.pi * 2
-            rn = random.random()
+            rn = np.random.rand(5)
+            angle = rn[0]  * np.pi * 2
             if (directionDim == 0 ):
-                coords[0] = center[0] + height * random.random()
-                coords[1] = center[1] +radius * np.cos(angle) * rn
-                coords[2] = center[2] +radius * np.sin(angle) * rn
+                coords[0] = center[0] + height * rn[1]
+                coords[1] = center[1] +radius * cos(angle) * rn[4]
+                coords[2] = center[2] +radius * sin(angle) * rn[4]
             if (directionDim == 1):
-                coords[0] = center[0] +radius * np.cos(angle) * rn
-                coords[1] = center[1] +height * random.random()
-                coords[2] = center[2] +radius * np.sin(angle) * rn
+                coords[0] = center[0] +radius * cos(angle) * rn[4]
+                coords[1] = center[1] +height * rn[2]
+                coords[2] = center[2] +radius * sin(angle) * rn[4]
             if (directionDim == 2):
-                coords[0] = center[0] +radius * np.cos(angle) * rn
-                coords[1] = center[1] +radius * np.sin(angle) * rn
-                coords[2] = center[2] +height * random.random()
+                coords[0] = center[0] +radius * cos(angle) * rn[4]
+                coords[1] = center[1] +radius * sin(angle) * rn[4]
+                coords[2] = center[2] +height * rn[3]
 
 
 
@@ -178,16 +182,18 @@ def generateNodesOrtoCilinder3dRand_cython(
 
 
 
-
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+@cython.cdivision(True)
 def checkMutDistancesLoops_cython(int dim,
                                   float minDist,
                                   list currentNodes,
                                   list newNode):
     cdef:
-        distIsGood = True
+        bool distIsGood = True
         int p, d
         int currentNodes_len = len(currentNodes)
-        double distInt
+        double distInt, dx
 
     for p in range (currentNodes_len):
         distInt = 0
@@ -220,13 +226,13 @@ def generateNodesRectPeriodic_cython(double[:] maxLim,
         int generatedPoints = 0
         int p, d
         int tr = 0
-        double distInt
+        double distInt, dx
         vector[double] coords
         int node_coords_input_len = len(node_coords)
         vector[double] node_coords_temp
         bint distIsGood
-        mt19937_64 gen = mt19937_64()
-        uniform_real_distribution[double] dist = uniform_real_distribution[double](0.0, 1.0)
+        #mt19937_64 gen = mt19937_64()
+        #uniform_real_distribution[double] dist = uniform_real_distribution[double](0.0, 1.0)
     for d in range(dim):
         coords.push_back(0.0)
 
@@ -242,7 +248,7 @@ def generateNodesRectPeriodic_cython(double[:] maxLim,
             #for d in range(dim):
             #    coords[d] = dist(gen) * maxLim[d]
             for d in range(dim):
-                 coords[d] = random.random() * maxLim[d]
+                 coords[d] = np.random.rand() * maxLim[d]
             distIsGood = True
 
             for p in range(node_coords_input_len + generatedPoints):
@@ -251,10 +257,10 @@ def generateNodesRectPeriodic_cython(double[:] maxLim,
                     dx = node_coords_temp[p*dim+d]
                     dx -= coords[d]
 
-                    if (np.abs(dx+maxLim[d]) < np.abs(dx) ):
+                    if (abs(dx+maxLim[d]) < abs(dx) ):
                         dx += maxLim[d]
 
-                    elif (np.abs(dx-maxLim[d]) < np.abs(dx) ):
+                    elif (abs(dx-maxLim[d]) < abs(dx) ):
                         dx -= maxLim[d]
 
 
@@ -285,22 +291,24 @@ def generateNodesRectPeriodic_cython(double[:] maxLim,
 
 
 
-
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+@cython.cdivision(True)
 def checkMutDistancesLoopsPeriodic_cython(int dim, float minDist, list currentNodes, list newNode, double[:] maxLim):
     cdef:
-        distIsGood = True
+        bool distIsGood = True
         int p, d
         int currentNodes_len = len(currentNodes)
-        double distInt
+        double distInt, dx
 
     for p in range (currentNodes_len):
         distInt = 0
         for d in range(dim):
             dx = currentNodes[p*dim+d] - newNode[d]
 
-            if (np.abs(dx+maxLim[d]) < np.abs(dx) ):
+            if (abs(dx+maxLim[d]) < abs(dx) ):
                 dx += maxLim[d]
-            elif (np.abs(dx-maxLim[d]) < np.abs(dx) ):
+            elif (abs(dx-maxLim[d]) < abs(dx) ):
                 dx -= maxLim[d]
 
             distInt += dx * dx
