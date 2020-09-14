7a9e86706f455bff1f9076130d4e6575840f915e
Vaclav Sadilek <kelidas@centrum.cz>
2020-09-14 13:55:29 +0200

    Eigen in thirparty; added tests; tests in docs

---
+++ src/tests/check.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import subprocess
+import pathlib
+import glob
+import filecmp
+
+def simple_file_compare(dir1, dir2):
+    ''' Compare files in dir1 with respect to dir2
+    '''
+    results_dir = pathlib.Path('results')
+    check_results_dir = pathlib.Path('check_results')
+    
+    if not (results_dir.exists() and check_results_dir.exists()):
+        print('One of result directories is missing!')
+        return 1
+    
+    files = check_results_dir.glob('*.*')
+    
+    identical = []
+    for f in files:
+        identical.append(filecmp.cmp(f, results_dir / f.name, shallow=False))
+    return not all(identical)
+    
+
+if __name__ == '__main__':
+    executable = sys.argv[1]
+    cwd = pathlib.Path.cwd()
+
+    #sample = subprocess.Popen([executable, os.path.join(cwd, 'master.inp')])
+    sample = subprocess.Popen([executable, 'master.inp'])
+    sample.wait()
+
+    if sample.returncode != 0:
+        sys.exit(1)
+    else:
+        simple_compare = simple_file_compare('check_results', 'results')
+        print(simple_compare)
+        sys.exit(simple_compare)
