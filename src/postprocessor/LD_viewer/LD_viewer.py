03566b31bf021b33c31dbbd3b28a359ce94d1c34
Vaclav Sadilek <kelidas@centrum.cz>
2020-10-14 21:37:52 +0200

    LD Viewer updated

--- src/postprocessor/LD_viewer/LD_viewer.py
+++ src/postprocessor/LD_viewer/LD_viewer.py
@@ -3,8 +3,10 @@ import pathlib
 import argparse
 os.environ["ETS_TOOLKIT"] = "qt"
 os.environ["QT_API"] = "pyqt5"
-from traits.api import HasStrictTraits, Instance, File, List, Enum, Button, Str, Any, Bool
-from traitsui.api import Item, Group, View, HSplit, NoButtons, EnumEditor, HGroup, Handler, VGroup, Tabbed, TextEditor, Label
+from traits.api import HasStrictTraits, Instance, File, List, Enum, Button, Str, \
+                        Any, Bool, DelegatesTo, Property, WeakRef
+from traitsui.api import Item, Group, View, HSplit, NoButtons, EnumEditor, HGroup,\
+                         Handler, VGroup, Tabbed, TextEditor, Label, ListEditor
 from traitsui.file_dialog  import open_file, TextInfo, FileInfo
 from mpl_qt_editor import MPLFigureEditor
 from matplotlib.figure import Figure
@@ -12,34 +14,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# HGroup Item hack
+def HItem(value=None, **traits):
+    return HGroup(Item(value, **traits, springy=True))
 
+class LDFile(HasStrictTraits):
+    ldfile_num = 0
+    
+    def __init__(self, **traits):
+        LDFile.ldfile_num += 1
+        super().__init__(**traits)
 
-class LDViewer(HasStrictTraits):
-    ld_file = Any(changed=True)
+    name = Property(Str)
+    ld_file = Any('', changed=True)
     open_button = Button('Open...')
     reload_button = Button('Reload')
-
-    figure = Instance(Figure)
-
     data = Any()
     labels = List([], changed=True)
-    x_values = Enum(values='labels')
-    y_values = Enum(values='labels')
 
-    draw_button = Button('Draw')
+    add_ldcurve = Button('Add new LD curve')
+    ld_curves = List
+    figure = Instance(Figure)
 
-    clear_axis = Bool(True)
+    def _add_ldcurve_fired(self):
+        self.ld_curves.append(LDCurve(ldfile=self, labels=self.labels, figure=self.figure))
 
-    def __init__(self, **traits):
-        super().__init__(**traits)
-        if pathlib.Path('LD.out').exists():
-            self.ld_file = 'LD.out'
-
-    def _figure_default(self):
-        figure = Figure()
-        #figure.add_axes([0.07, 0.05, 0.85, 0.92])
-        figure.add_subplot(111)
-        return figure
+    def _get_name(self):
+        return 'F{}'.format(self.ldfile_num)
 
     def _open_button_fired ( self ):
         """ Handles the user clicking the 'Open...' button.
@@ -60,28 +61,135 @@ class LDViewer(HasStrictTraits):
         self.data = pd.read_csv(self.ld_file, sep='\t', header=0, index_col=False)
         return list(self.data)
 
-    def _draw_button_fired(self):
-        ax = self.figure.axes[0]
-        if self.clear_axis:
-            ax.cla()
-        ax.plot(self.data[self.x_values], self.data[self.y_values])
-        self.figure.canvas.draw()
-    
+    def _labels_changed(self):
+        for curve in self.ld_curves:
+            curve.labels = self.labels
+
     view = View(VGroup(HGroup(Item('open_button', show_label=False, id='ld_open'),
                        Item('reload_button', show_label=False),
-                       Item('ld_file', id='ld_file', style='readonly')
+                       Item('ld_file', id='ld_file'),#, style='readonly')
                         ),
-                HSplit(Group(
-                             Item('x_values'),
-                             Item('y_values'),
-                             Item('clear_axis'),
-                             Item('draw_button', show_label=False),
-                             id='ld_group',
-                             ),
+                Item('add_ldcurve', show_label=False),
+                Item('ld_curves', style='custom', show_label=False,
+                editor=ListEditor(use_notebook=True,
+                               deletable=True,
+                               dock_style='tab',
+                               page_name='.name')),
+                ))
+
+class LDCurve(HasStrictTraits):
+    ld_num = 0
+    
+    def __init__(self, **traits):
+        LDCurve.ld_num += 1
+        super().__init__(**traits)
+
+    ldfile = Instance(LDFile)
+    labels = List
+
+    name = Property(Str)
+    
+    x_values = Enum(values='labels')
+    y_values = Enum(values='labels')
+
+    figure = Instance(Figure)
+    draw_this_button = Button('Draw this one')
+
+    def _draw_this_button_fired(self):
+        ax = self.figure.axes[0]
+        self._draw_this(ax)
+        self.figure.canvas.draw()
+
+    def _draw_this(self, ax):
+        #if self.clear_axis:
+        #   ax.cla()
+        ax.plot(self.ldfile.data[self.x_values], self.ldfile.data[self.y_values], 
+                label='{}-{}'.format(self.ldfile.name, self.name))
+        ax.legend()
+
+    def _get_name(self):
+        return 'LD {}'.format(self.ld_num)
+
+    view = View(VGroup(HItem('x_values'),
+                       HItem('y_values'),
+                       Item('draw_this_button', show_label=False))
+                )
+
+
+class LDFiles(HasStrictTraits):
+    figure = Instance(Figure)
+    add_ldfile = Button('Add new LD file')
+    ldfiles = List  #(Instance('LDFile'))
+
+    def _add_ldfile_fired(self):
+        print('XXX', self.figure)
+        self.ldfiles.append(LDFile(figure=self.figure))
+
+    view = View(
+        Item('add_ldfile', show_label=False),
+        Item('ldfiles', style='custom', show_label=False,
+             editor=ListEditor(use_notebook=True,
+                               deletable=True,
+                               dock_style='tab',
+                               page_name='.name')),
+        resizable=True
+    )
+
+
+class ControlPanel(HasStrictTraits):
+    ldfiles = Instance(LDFiles)
+    figure = Instance(Figure)
+
+    draw_button = Button('Draw all')
+    clear_button = Button('Clear')
+
+    #clear_axis = Bool(True)
+
+    def _ldfiles_default(self):
+        print(self.figure)
+        return LDFiles(figure=self.figure)
+
+    def _clear_button_fired(self):
+        ax = self.figure.axes[0]
+        ax.cla()
+        self.figure.canvas.draw()
+
+    def _draw_button_fired(self):
+        for lds in self.ldfiles.ldfiles:
+            for ld in lds.ld_curves:
+                ld.draw_this_button = True
+
+    view = View((Item('@ldfiles', show_label=False),
+                    HGroup(
+                       Item('clear_button', show_label=False, springy=True),
+                       Item('draw_button', show_label=False, springy=True)),),
+                resizable=True
+            )
+
+
+class LDViewer(HasStrictTraits):
+    
+    panel = Instance(ControlPanel)
+    figure = Instance(Figure)
+
+    def _figure_default(self):
+        figure = Figure()
+        #figure.add_axes([0.07, 0.05, 0.85, 0.92])
+        figure.add_subplot(111)
+        return figure
+
+    def _panel_default(self):
+        return ControlPanel(figure=self.figure)
+
+    def __init__(self, **traits):
+        super().__init__(**traits)
+        if pathlib.Path('LD.out').exists():
+            self.ld_file = 'LD.out'
+    
+    view = View( HSplit('@panel',
                        Item('figure', show_label=False, editor=MPLFigureEditor(), dock='vertical', width=0.8),
                        show_labels=False, id='ld_hsplit', dock='tab'
                       ),
-                ),
                 title='LD Viewer',
                 resizable=True,
                 height=0.75, width=0.75,
@@ -117,6 +225,20 @@ def init_parser():
 
 
 if __name__ == '__main__':
+    args = init_parser()
+
+    #LDFiles().configure_traits(filename='test.out')
+    ld_viewer = LDViewer()
+    if args.ld_file:
+        ld_viewer.panel.ldfiles.add_ldfile = True
+        ld_viewer.panel.ldfiles.ldfiles[0].ld_file = args.ld_file
+        ld_viewer.panel.ldfiles.ldfiles[0].add_ldcurve = True
+    ld_viewer.configure_traits()
+
+
+    exit()
+
+
     args = init_parser()
 
     if args.ld_file:
---
+++ src/postprocessor/LD_viewer/README.md
@@ -0,0 +1,11 @@
+# LD Viewer
+Simple viewer for LD.out files.
+- example.bat for Windows
+- example ld_viewer.desktop for linux context menu
+  - in KDE Plasma desktops place in /home/username/.local/share/kservices5/ServiceMenus
+
+## Requirments:
+- traits
+- traitsui
+- pandas
+- matplotlib
