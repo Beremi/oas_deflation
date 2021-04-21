781759b28db959e00046683e0cc9eaf519307b4f
Jose <jose.kvet@gmail.com>
2021-04-21 21:56:30 +0200

    mars help variable removal

--- src/solver/src/mars_material.cpp
+++ src/solver/src/mars_material.cpp
@@ -176,7 +176,7 @@ void MarsMaterialStatus :: computeDamage(Vector strain) {
     if ( temp_damage < damage ) {
         temp_damage = damage;
     }
-    temp_damage = min(temp_damage, 0.999); 
+    // temp_damage = min(temp_damage, 0.999);  // dangerous, do not use
 
     //temp_crackOpening = (L*damage)*strain[0]; //normal opening only
     temp_crackOpening = l2_norm( ( L * damage ) * strain ); //total opening
